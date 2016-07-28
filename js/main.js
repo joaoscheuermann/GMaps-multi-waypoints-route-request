5ee25249527c937379440496c6a2342b65918f4a
Joao Vitor Scheuermann <jvitor.sche@gmail.com>
2016-07-28 11:32:10 -0300

    Adicionado pastas para organização

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const teste = new multiRouteRequest();
+
+let map;
+let directionsService;
+
+function init() {
+    let mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 3
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+window.onload = () => {
+
+    init();
+    directionsService = new google.maps.DirectionsService();
+
+    let wp = []
+
+    console.log("A click adds a wp in the array!");
+    map.addListener('click', (e) => {
+        console.log({
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng()
+        }, wp.length);
+
+        wp.push({
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng()
+        });
+
+
+    })
+
+    console.log("A double click calls the calcRoute method, process the data and return a array with all the ordened results");
+    map.addListener('dblclick', (e) => {
+        teste.calcRoute(wp, 10, (data, status) => {
+            if (status == 'READY') {
+                console.log(status, data);
+            }
+        })
+        wp = [];
+    });
+
+
+
+
+
+
+}
---
+++ js/route_generation.js
@@ -0,0 +1,138 @@
+function multiRouteRequest() {
+
+    function logger(toLog) {
+      
+    }
+    //Agroup the array to a max given number.
+    function agroupArray(data, max_gs) {
+        let max_group_size = max_gs;
+        let lastPos = 0;
+        let returnData = [];
+        if (data != undefined && data.length > 0) {
+            for (let i = 0; i < data.length + max_group_size; i += max_group_size) {
+                if (i > 0) {
+                    let arrayCoords = []
+                    for (let j = lastPos; j < i; j++) {
+                        if (data[j] !== undefined) {
+                            arrayCoords.push(data[j]);
+                        }
+                    }
+                    returnData.push(arrayCoords);
+                    lastPos = i;
+                }
+            }
+        }
+        return returnData;
+    };
+
+    //Format the data to send the request;
+    function formatData(data) {
+        let returnData = []
+        for (let i in data) {
+            let start = data[i].shift();
+            let end = data[i].pop();
+            let wp = []; //Waypoints
+            for (let j in data[i]) {
+                wp.push({
+                    location: data[i][j],
+                    stopover: false
+                })
+            }
+            returnData.push({
+                start: start,
+                end: end,
+                wp: wp
+            });
+        }
+        return returnData;
+    }
+
+    //Faz as solicitaçoes e organiza os resultados em ordem crescente.
+    function calcRoute(data, callback) {
+
+        let recived_data = [];
+
+        for (let i in data) {
+            let request = {
+                origin: data[i].start,
+                destination: data[i].end,
+                waypoints: data[i].wp,
+                travelMode: google.maps.TravelMode.DRIVING
+            };
+            directionsService.route(request, (result, status) => {
+                if (status == google.maps.DirectionsStatus.OK) {
+                    recived_data.push({
+                        result: result,
+                        id: i
+                    });
+                    passResult();
+                } else {
+                    console.log(status);
+                    return
+                }
+            });
+        }
+
+        function passResult() {
+            if (recived_data.length === data.length) {
+                let return_data = [];
+                let ordened_data = recived_data.sort((a, b) => {
+                    return a.id - b.id
+                });
+                for (let l in ordened_data) {
+                  return_data.push(ordened_data[l].result)
+                }
+                callback(return_data, 'READY');
+            } else {
+                callback(null, 'WAIT');
+            }
+        }
+    }
+
+
+    //COORDS INPUT =>  {lat: xxx, lng: xxx}
+    this.calcRoute = (wp, max_gs, callback) => {
+      let formated_data = formatData(agroupArray(wp, max_gs))
+      calcRoute(formated_data, (data, status) => {
+          callback(data, status);
+      });
+    }
+}
+
+
+
+/*
+function onDone() {
+    let marker_pos = getAllMarkerPos();
+    let agrouped_array = agroupArray(marker_pos, 10);
+    let formated_data = formatData(agrouped_array);
+    calcRoute(formated_data, (data, status) => {
+        if (status == 'OK') {
+            let route = {
+                id: '',
+                name: bar_center.name,
+                settings: false,
+                data: {
+                    coords: data
+                }
+            }
+            routes_cache.push(route);
+            bar_center.cancel();
+        }
+    });
+}
+
+//Estrair apenas as coordenadas da rota.
+let path = result.routes[0].legs[0].steps;
+
+let coords_cache = [];
+for (let j in path) {
+    let rr = path[j].lat_lngs //google.maps.geometry.encoding.decodePath(path[j].encoded_lat_lngs);
+    for (var k in rr) {
+        coords_cache.push({
+            lat: rr[k].lat(),
+            lng: rr[k].lng()
+        });
+    }
+}
+*/
